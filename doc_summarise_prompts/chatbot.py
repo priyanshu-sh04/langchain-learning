ac37d23b719fb2a9bb1a1a9c68a4c15efa11827a
Priyanshu <sharmapriyanshu29272@gmail.com>
2026-07-06 00:03:23 +0530

    chat prompt Templates

---
+++ doc_summarise_prompts/chatbot.py
@@ -0,0 +1,28 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id="meta-llama/Llama-3.1-8B-Instruct",
+    task="text-generation",
+    temperature=0.9,
+)
+
+model = ChatHuggingFace(llm = llm)
+
+chat_history = [
+    SystemMessage(content='You are helpful Ai assistant')
+]
+
+while True :
+    user_input = input('You: ')
+    chat_history.append(HumanMessage(content=user_input))
+    if user_input == 'exit':
+        break
+    result = model.invoke(chat_history)
+    chat_history.append(AIMessage(content = result.content))
+    print("AI :", result.content)
+
+print(chat_history)
